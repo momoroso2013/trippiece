24f14028f8a8aa8613c49b750db749aa964fa72f
momoroso2013 <keitamomotani@Keita-no-MacBook-Air.local>
2017-06-05 11:15:08 +0900

    create project model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170603023145) do
+ActiveRecord::Schema.define(version: 20170603103735) do
+
+  create_table "projects", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "title",       limit: 65535, null: false
+    t.text     "description", limit: 65535, null: false
+    t.string   "image"
+    t.string   "place"
+    t.integer  "price"
+    t.integer  "start_at"
+    t.integer  "end_at"
+    t.text     "detail",      limit: 65535
+    t.integer  "user_id"
+    t.integer  "status",                    null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.index ["user_id"], name: "index_projects_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                                              null: false
@@ -33,4 +49,5 @@ ActiveRecord::Schema.define(version: 20170603023145) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "projects", "users"
 end
