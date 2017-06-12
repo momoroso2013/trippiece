2518e2c0ebfffb59832f26c07cbe4c9c4d23c7fb
momoroso2013 <keitamomotani@Keita-no-MacBook-Air.local>
2017-06-12 20:51:34 +0900

    ビューのクオリティをあげる
    ボタンを押すと並び替えの候補を出す。
    マウスを乗せると光ったように見せる。
    画像を横にスライドさせる

---
+++ app/assets/javascripts/project.js
@@ -0,0 +1,23 @@
+$(function() {
+  $('.slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: false
+  });
+
+  $('.contents__pickup__sub li').mouseover(function(){
+    $(this).css('opacity', '0.6').animate({'opacity': '1'}, 'slow');
+  });
+
+  $('.contents__main__sort__recommend').on("click", function() {
+    $("#select-1").slideToggle("slow");
+    });
+
+  $('.contents__main__sort__project-status').on("click", function() {
+    $("#select-2").slideToggle("slow");
+    });
+});
