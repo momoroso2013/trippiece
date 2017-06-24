c89a1462faeaa54a1fdb63718a8bdf82640904ac
momoroso2013 <keitamomotani@Keita-no-MacBook-Air.local>
2017-06-24 17:59:11 +0900

    likeモデルの作成、アソシエーションを組む

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :projects
+  has_many :likes
+  has_many :projects, through: :likes
   has_many :participants
   mount_uploader :image, ImageUploader
 end
