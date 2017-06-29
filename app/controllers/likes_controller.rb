33a29a66966d10240954828c23ad7d215a512f9d
momoroso2013 <peachvalley1019@yahoo.co.jp>
2017-06-29 19:23:52 +0900

    非同期にするため、コントローラの記述の修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,16 +3,12 @@ class LikesController < ApplicationController
     @user_id = current_user.id
     @project = Project.find(params[:project_id])
     @like = Like.new(project_id: @project.id, user_id: @user_id)
-    if @like.save
-      redirect_to project_path(@project)
-    end
+    @like.save
   end
 
   def destroy
-    @like = Like.find(params[:id])
+    like = Like.find(params[:id])
     @project = Project.find(params[:project_id])
-    if @like.destroy
-    redirect_to project_path(@project)
-    end
+    like.destroy
   end
 end
